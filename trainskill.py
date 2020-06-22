a2568b2c6b89445695572c63d6c4422e3eca5d4e
chichi <jang.chihun@gmail.com>
2020-06-22 16:36:24 +0900

    [6장] 학습관련기술

---
+++ trainskill.py
@@ -0,0 +1,44 @@
+class SGD:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+
+    def update(self, params, grads):
+        for key in params.keys():
+            params[keys] -= self.lr = grads[key]
+
+
+class Momentum:
+    def __init__(self, lr=0.01, momentum=0.9):
+        self.lr = lr
+        self.momentum = momentum
+        self.v = None
+
+    def updata(self, params, grads):
+        if self.v is None:
+            self.v = {}
+            for key, val in params.items():
+                self.v[key] = np.zeros_like(val)
+
+        for key in params.keys():
+            self.v[key] = self.momentum*self.v[key] - self.lr*grads[key]
+            params[key] += self.v[key]
+
+# 인스턴스 변수 v는 물체의 속도이다.
+# update()가 호출될때 매개변수와 같은 구조의 data를 딕셔너리 변수로 저장
+
+
+class AdaGrad:
+    def __init__(self, lr=0.01):
+        self.lr = lr
+        self.h = None
+
+    def update(self, params, grads):
+        if self.h is None:
+            self.h = {}
+            for key, val in params.items():
+                self.h[key] = np.zeros_like(val)
+        for key in params.keys():
+            self.h[key] += grads[key] * grads[key]
+            params[key] -= self.lr * grads[key] / (np.sqrt(self.h[ey]) + 1e-7)
+
+    # 마지막에 1e-7을 더하는 부분은 self.h[key]가 0이라해도 0으로 나누는 사태를 막는다.
